c32e84f04b071e1dfe0bf19f4ae91d72e06c36ab
Peter Marsh <p.marsh@mac.com>
2018-03-13 12:54:51 -0400

    first

---
+++ main.js
@@ -0,0 +1,45 @@
+var coin = {
+    state: 0,
+    flip: function () {
+        // randomly set this.state to be either 0 or 1
+        this.state = Math.floor(Math.random() * 2);
+        return this.state;
+    },
+    toString: function () {
+        // return “H” or “T” depending on whether this.state is 0 or 1
+        switch (this.state) {
+            case 0:
+                this.state = "H";
+                break;
+            case 1:
+                this.state = "T";
+        }
+        return this.state;
+    },
+    toHTML: function () {
+        var img = document.createElement("img");
+        // set the properties of the image element to show either heads or tails
+        switch (this.state) {
+            case "H":
+                img.setAttribute("id", "heads");
+                img.setAttribute("src", "photos/pepsi_logo.png");
+                document.body.appendChild(img);
+                break;
+            case "T":
+                img.setAttribute("id", "tails");
+                img.setAttribute("src", "photos/coca-cola-logo.png");
+                document.body.appendChild(img);
+        };
+        return img;
+    }
+};
+
+for (i = 1; i < 21; i++) {
+    console.log(i + ": State - " + coin.state);
+    console.log(i + ": Flip - " + coin.flip());
+    console.log(i + ": String - " + coin.toString());
+    console.log(i + ": html " + coin.toHTML());
+};
+
+
+
